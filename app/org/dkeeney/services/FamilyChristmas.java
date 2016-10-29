30de7363d2635ad5680bb88964ed3cafab0bbe83
Daniel Keeney <daniel.keeney@gmail.com>
2016-10-29 22:08:12 +0800

    Add rules to avoid gifting your spouse

--- app/org/dkeeney/services/FamilyChristmas.java
+++ app/org/dkeeney/services/FamilyChristmas.java
@@ -7,54 +7,102 @@ import org.dkeeney.models.Gender;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.dkeeney.models.AgeGroup.ADULT;
+import static org.dkeeney.models.Gender.*;
 
 public class FamilyChristmas {
   private final List<FamilyMember> familyMembers;
+  private final Random random;
 
   @Inject
-  public FamilyChristmas(FamilyDao familyDao) throws IOException {
+  public FamilyChristmas(FamilyDao familyDao, Random random) throws IOException {
     familyMembers = familyDao.loadFamily();
+    this.random = random;
   }
 
-  public Set<FamilyMember> filterAge(AgeGroup ageGroup) {
+  public List<FamilyMember> filterAge(AgeGroup ageGroup) {
     return filterAge(ageGroup, familyMembers);
   }
 
-  private Set<FamilyMember> filterAge(AgeGroup ageGroup, List<FamilyMember> initialSet) {
+  private List<FamilyMember> filterAge(AgeGroup ageGroup, List<FamilyMember> initialSet) {
     return initialSet.stream()
         .filter(member -> ageGroup.equals(member.getAgeGroup()))
-        .collect(Collectors.toSet());
+        .collect(Collectors.toList());
   }
 
-  public Set<FamilyMember> filterGender(Gender gender) {
+  public List<FamilyMember> filterGender(Gender gender) {
     return filterGender(gender, familyMembers);
   }
 
-  private Set<FamilyMember> filterGender(Gender gender, List<FamilyMember> initialSet) {
+  private List<FamilyMember> filterGender(Gender gender, List<FamilyMember> initialSet) {
     return initialSet.stream()
         .filter(member -> gender.equals(member.getGender()))
-        .collect(Collectors.toSet());
+        .collect(Collectors.toList());
   }
 
   public Map<FamilyMember, FamilyMember> assignAdults() {
     Map<FamilyMember, FamilyMember> ret = new HashMap<>();
-    Set<FamilyMember> givers = filterAge(ADULT);
-    Set<FamilyMember> receivers = filterAge(ADULT);
-    Iterator<FamilyMember> giverator = givers.iterator();
-    Iterator<FamilyMember> receiverator = receivers.iterator();
+    List<FamilyMember> givers = filterAge(ADULT);
+    List<FamilyMember> receivers = filterAge(ADULT);
+    Collections.shuffle(givers, random);
+    Collections.shuffle(receivers, random);
 
-    while (giverator.hasNext() && receiverator.hasNext()) {
-      ret.put(giverator.next(), receiverator.next());
-    }
+    ret = assignAdultsRecursive(ret, givers, receivers);
 
     return ret;
+  }
+
+  private Map<FamilyMember, FamilyMember> assignAdultsRecursive(
+      Map<FamilyMember, FamilyMember> results, List<FamilyMember> givers, List<FamilyMember> receivers) {
+    if (givers.isEmpty()) {
+      return results;
+    }
+    FamilyMember giver = givers.get(0);
+    for (int i = 0; i < receivers.size(); i++) {
+      FamilyMember receiver = receivers.get(i);
+      if (validGiftingPair(giver, receiver)) {
+        Map<FamilyMember, FamilyMember> newResults = new HashMap<>(results);
+        newResults.put(giver, receiver);
+        List<FamilyMember> newGivers = removeMember(givers, giver);
+        List<FamilyMember> newReceivers = removeMember(receivers, receiver);
+        Map<FamilyMember, FamilyMember> nextAttempt = assignAdultsRecursive(newResults, newGivers, newReceivers);
+        if (nextAttempt != null) {
+          return nextAttempt;
+        }
+      }
+    }
+    return null;
+  }
+
+  private boolean validGiftingPair(FamilyMember giver, FamilyMember receiver) {
+    if (giver.getShortName().equals(receiver.getShortName())) {
+      // no self giving!
+      return false;
+    }
+    if (giver.getSpouse().equals(receiver.getShortName())) {
+      // no giving to you spouse!
+      return false;
+    }
+    if (giver.getShortName().equals("Acha") && receiver.getGender() == FEMALE) {
+      // let's make it easier for him
+      return false;
+    }
+
+    return true;
+  }
+
+  private List<FamilyMember> removeMember(List<FamilyMember> familyMembers, FamilyMember toRemove) {
+    return familyMembers.stream()
+        .filter(member -> !member.getShortName().equals(toRemove.getShortName()))
+        .collect(Collectors.toList());
   }
 }
--- test/org/dkeeney/services/FamilyChristmasTest.java
+++ test/org/dkeeney/services/FamilyChristmasTest.java
@@ -3,19 +3,18 @@ package org.dkeeney.services;
 import org.dkeeney.dao.FamilyDao;
 import org.dkeeney.models.FamilyMember;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Random;
+import java.util.function.Supplier;
 
 import static org.dkeeney.models.AgeGroup.ADULT;
 import static org.dkeeney.models.AgeGroup.CHILD;
 import static org.dkeeney.models.Gender.FEMALE;
 import static org.dkeeney.models.Gender.MALE;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class FamilyChristmasTest {
   private FamilyChristmas familyChristmas;
@@ -24,8 +23,21 @@ public class FamilyChristmasTest {
   @Before
   public void before() throws IOException {
     FamilyDao familyDao = new FamilyDao();
-    familyChristmas = new FamilyChristmas(familyDao);
+    familyChristmas = new FamilyChristmas(familyDao, new Random(3));
     adultExchange = familyChristmas.assignAdults();
+  }
+
+  private void repeatTest(int repeats, Supplier<Void> test) {
+    for (int i = 0; i < repeats; i++) {
+      test.get();
+    }
+  }
+
+  private void assertAdultExchange(Map<FamilyMember, FamilyMember> adultExchange) {
+    assertNotNull("Adult exchanges should be defined", adultExchange);
+    assertEquals("Not all adults are giving gifts", 18, adultExchange.size());
+    adultExchange.forEach((giver, receiver) ->
+        assertEquals("Only adults should be involved in the adult exchange", ADULT, receiver.getAgeGroup()));
   }
 
   @Test
@@ -41,17 +53,47 @@ public class FamilyChristmasTest {
   }
 
   @Test
-  public void testAssignAdultsIncludesEveryone() {
-    assertNotNull("Adult exchanges should be defined", adultExchange);
-    assertEquals("Not all adults are giving gifts", 18, adultExchange.size());
+  public void testAssignAdultsHasNoSelfGiving() {
+    repeatTest(100, () -> {
+      adultExchange = familyChristmas.assignAdults();
+
+      adultExchange.forEach((giver, receiver) ->
+          assertNotEquals("Should not be giving gifts to yourself",
+              giver,
+              receiver));
+      assertAdultExchange(adultExchange);
+      return null;
+    });
   }
 
-  @Ignore
   @Test
-  public void testAssignAdultsHasNoSelfGiving() {
-    adultExchange.entrySet().forEach(entry ->
-        assertNotEquals("Should not be giving gifts to yourself",
-            entry.getKey(),
-            entry.getValue()));
+  public void testAssignAdultsHasNoSpouseGiving() {
+    repeatTest(100, () -> {
+      adultExchange = familyChristmas.assignAdults();
+
+      adultExchange.forEach((giver, receiver) ->
+        assertNotEquals("Should not be giving to your spouse",
+            giver.getSpouse(),
+            receiver.getShortName()));
+      assertAdultExchange(adultExchange);
+      return null;
+    });
+  }
+
+  @Test
+  public void testAssignAdultsGivesAchaMaleRecipient() {
+    repeatTest(100, () -> {
+      adultExchange = familyChristmas.assignAdults();
+
+      adultExchange.entrySet().stream()
+          .filter(entry -> entry.getKey().getShortName().equals("Acha"))
+          .forEach(entry -> {
+            assertEquals("Subramaniam should be gifting men",
+                MALE,
+                entry.getValue().getGender());
+          });
+      assertAdultExchange(adultExchange);
+      return null;
+    });
   }
 }
